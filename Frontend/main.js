928bd4794700703dda8e0fdb734f186be543129c
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-10-04 12:37:07 -0600

    "Adding OpenAi API key"

--- Frontend/main.js
+++ Frontend/main.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = ""; // Your OpenAI API key here
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
